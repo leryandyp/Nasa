5d628fa17d789f57a45c58a03a76d96d7d1b719f
leryandy <45796718+leryandyp@users.noreply.github.com>
2020-05-10 17:58:13 -0400

    Fecha2

--- fecha.php
+++ fecha.php
@@ -1,11 +1,21 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fecha</title>
+</head>
+<body>
 <?php
 date_default_timezone_set('America/La_Paz');
-//$hora = date("H:i:s");   
 $hoy = date("j-n-Y");  
 echo $hoy;
-// echo "<br>";
-// echo $hora;
 ?>
+ <!-- echo "<br>";
+ echo $hora;
+$hora = date("H:i:s");   -->
+</body>
+</html>
+
 
 
